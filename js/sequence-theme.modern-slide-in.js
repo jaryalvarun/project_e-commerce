f59f4fd60f543287fce0b307def0fe38a55802a7
kaushik parmar <74057599+white-devil-hacker@users.noreply.github.com>
2021-10-22 12:36:39 +0530

    Initial commit

---
+++ js/sequence-theme.modern-slide-in.js
@@ -0,0 +1,25 @@
+/**
+ * Theme Name: Modern Slide In
+ *
+ * A minimalist theme for showcasing products
+ *
+ * This theme is powered by Sequence.js - The
+ * responsive CSS animation framework for creating unique sliders,
+ * presentations, banners, and other step-based applications.
+ *
+ */
+
+// Get the Sequence element
+var sequenceElement = document.getElementById("sequence");
+
+// Place your Sequence options here to override defaults
+// See: http://sequencejs.com/documentation/#options
+var options = {
+  animateCanvas: false,
+  phaseThreshold: false,
+  preloader: true,
+  reverseWhenNavigatingBackwards: true
+}
+
+// Launch Sequence on the element, and with the options we specified above
+var mySequence = sequence(sequenceElement, options);
